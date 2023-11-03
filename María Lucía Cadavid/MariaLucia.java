01d7d949a5b265e5918927340731d66e48243724
luu1003 <cadavid.mmaria.l@gmail.com>
2023-11-03 07:39:13 -0500

    1

---
+++ María Lucía Cadavid/MariaLucia.java
@@ -0,0 +1,15 @@
+package enClase;
+import java.io.*;
+import java.nio.file.*;
+
+  public class MariaLucia {
+    public static void main(String[] args){
+
+
+      int id, amount, tipo, tipoE, tipoR, hora;
+     
+
+    }
+
+
+}
