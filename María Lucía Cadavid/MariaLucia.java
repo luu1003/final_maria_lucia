873143b469f951595858681563c0de07a8404cb5
luu1003 <cadavid.mmaria.l@gmail.com>
2023-11-03 07:54:29 -0500

    3

--- María Lucía Cadavid/MariaLucia.java
+++ María Lucía Cadavid/MariaLucia.java
@@ -1,4 +1,5 @@
-mport java.util.HashMap;
+package enClase;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
@@ -14,3 +15,36 @@ public class MariaLucia {
     String[][] lunesd, martesd, miercolesd, juevesd, viernesd, sabadod,
         domingod = new String[100][5];
 
+
+    for (int i = 0; i < 100; i++) {
+      FileReader fr = null;
+      BufferedReader br = null;
+      try {
+        bufferedReader fr = new FileReader(diaSemana[i] + ".txt");
+        String linea = "id;amount;tipoE;tipoR;hora";
+        linea.split(";");
+        while ((linea = br.readLine()) != null) {
+          System.out.println(linea);
+        }
+
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+        System.out.println("no se puede leer el archivo");
+      } finally {
+        try {
+          if (null != fr) {
+            fr.close();
+          }
+        } catch (Exception e2) {
+          System.out.println(e2.getMessage());
+          ;
+
+          if
+
+        }
+      }
+    }
+    Scanner.close();
+  }
+
+}
