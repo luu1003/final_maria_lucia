0998efe2220a6a1c7dac1b433f5b950f42cfe72e
luu1003 <cadavid.mmaria.l@gmail.com>
2023-11-03 07:45:22 -0500

    Update MariaLucia.java

--- María Lucía Cadavid/MariaLucia.java
+++ María Lucía Cadavid/MariaLucia.java
@@ -1,15 +1,16 @@
-package enClase;
+mport java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Map.Entry;
 import java.io.*;
-import java.nio.file.*;
 
-  public class MariaLucia {
-    public static void main(String[] args){
+public class MariaLucia {
 
+  public static void main(String[] args) throws Exception {
+    Scanner scanner = new Scanner(System.in);
 
-      int id, amount, tipo, tipoE, tipoR, hora;
-     
+    String[] diaSemana = { "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo" };
+    String[][] lunesd, martesd, miercolesd, juevesd, viernesd, sabadod,
+        domingod = new String[100][5];
 
-    }
-
-
-}
